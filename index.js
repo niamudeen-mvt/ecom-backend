4dd3a18c92de895ac189f8d883e341a8e57a6cdb
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-02 17:48:15 +0530

    message:  cors setup

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./router/index.js");
 const connectDb = require("./utils/db.js");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors());
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-
 app.use(express.json());
-app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/v1", router);
 
